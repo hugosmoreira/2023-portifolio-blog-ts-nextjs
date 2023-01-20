dc07228d73aacf59669edcdd23bb3a9ccbcc6b2e
Hugo Moreira <hugo.n4@gmail.com>
2023-01-19 18:08:21 -0800

    update md.ts

--- lib/md.ts
+++ lib/md.ts
@@ -3,13 +3,25 @@ import fs from 'fs'
 
 const getDir = (path: string) => join(process.cwd(), path)
 
+const BLOG_DIR = getDir("/content/blogs");
+
+
 const getFileNames = (dir: string): string[] => {
     return fs.readdirSync(dir)
 }
+
+const getBlogFileNames = () => {
+    return getFileNames(BLOG_DIR);
+  }
 
 const getItemInPath = (filePath: string) => {
     const fileContent = fs.readFileSync(filePath, 'utf8');
     return fileContent
 }
 
-export { getFileNames, getDir, getItemInPath }
+const getBlog = (fileName: string) => {
+    const blog = getItemInPath(join(BLOG_DIR, fileName));
+    return blog;
+  }
+
+export { getFileNames, getDir, getItemInPath, getBlogFileNames, getBlog }
